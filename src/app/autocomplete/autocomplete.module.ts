8a06950b4ddf3981abd91fa1d0384d190ba62431
Viktoriya-Shchutska <viktoriya.shchutska@drillinginfo.com>
2021-05-16 20:21:30 +0200

    add mat-design autocomplete

--- src/app/autocomplete/autocomplete.module.ts
+++ src/app/autocomplete/autocomplete.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 
 @NgModule({
   declarations: [AutocompleteComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatAutocompleteModule
   ],
   exports: [AutocompleteComponent]
 })
