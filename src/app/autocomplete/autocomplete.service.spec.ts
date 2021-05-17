bf13c3f049abc574ea4a02ba3c37b8cb8bb0bb0a
Viktoriya-Shchutska <viktoriya.shchutska@drillinginfo.com>
2021-05-17 22:45:00 +0200

    tests

--- src/app/autocomplete/autocomplete.service.spec.ts
+++ src/app/autocomplete/autocomplete.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AutocompleteService } from './autocomplete.service';
 
@@ -6,7 +7,9 @@ describe('AutocompleteService', () => {
   let service: AutocompleteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ 
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AutocompleteService);
   });
 
--- src/app/autocomplete/autocomplete/autocomplete.component.spec.ts
+++ src/app/autocomplete/autocomplete/autocomplete.component.spec.ts
@@ -1,4 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 import { AutocompleteComponent } from './autocomplete.component';
 
@@ -8,7 +12,14 @@ describe('AutocompleteComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AutocompleteComponent ]
+      declarations: [AutocompleteComponent],
+      imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatAutocompleteModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   });
